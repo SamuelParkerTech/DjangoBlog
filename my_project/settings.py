ab554781c78441e3aa859f66adbdbe6254b35eb0
Samuel Parker <samuelparkertech@gmail.com>
2024-07-18 12:29:57 +0000

    getting files ready for Heroku deployment

--- my_project/settings.py
+++ my_project/settings.py
@@ -23,9 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-#aa-pd=u04ug3=kw-34rfv%uj2h49$m!__&@8(iw1n8-vysfkq'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['8000-samuelparker-djangorepo-lyrl0eo6ep1.ws.codeinstitute-ide.net']
+ALLOWED_HOSTS = ['8000-samuelparker-djangorepo-lyrl0eo6ep1.ws.codeinstitute-ide.net',
+                ,'.herokuapp.com']
 
 
 # Application definition
